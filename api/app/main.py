0d426cd4b0feb92a58649d3abe3534ee2d68a00f
student <student@deblauwe-yerl.mlops.local>
2021-12-14 13:29:28 +0100

    model fix attempt

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,10 @@
 from tensorflow import keras
+import numpy as np
+from skimage.transform import resize
+import matplotlib.pyplot as plt
 from fastapi import FastAPI, File
+from PIL import Image
+import io
 
 app = FastAPI()
 
@@ -7,5 +12,12 @@ model = keras.models.load_model('model/8522d1cc-40fc-4456-8ad2-4048dded0948.h5')
 
 @app.post("/test")
 async def create_file(file: bytes = File(...)):
-    pred = model.predict(file)
-    return {"message": pred}
+    image = np.array(Image.open(io.BytesIO(file)))
+    image = resize(image, (64,64,3))
+    plt.imsave('image.png', image)
+    print(image.shape)
+    image = np.array([image])
+    print(image.shape)
+
+    pred = model.predict(image)
+    return {"message": image.shape}
